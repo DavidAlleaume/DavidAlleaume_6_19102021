cef48d765b9ccc8c2e72583071fd73595c20359c
David Alleaume <david.alleaume@gmail.com>
2021-10-26 22:54:30 +0200

    Implémentation des routes et controllers Like et Dislike

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -3,7 +3,8 @@ const fs = require('fs')
 
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce)
-    delete sauceObject._id;
+    // on supprime l'ID du corps de la requête car Mongo va en créer un nouveau
+    delete sauceObject._id
     const sauce = new Sauce({
         ...sauceObject,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
@@ -11,7 +12,7 @@ exports.createSauce = (req, res, next) => {
         dislikes: 0,
         usersLiked: [' '],
         usersdisLiked: [' ']
-    });
+    })
     sauce
         .save()
         .then(() => res.status(201).json({ message: 'Votre sauce a bien été ajoutée !' }))
@@ -52,4 +53,41 @@ exports.getAllSauces = (req, res, next) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }))
+}
+
+exports.likesAndDislikesSauce = (req, res, next) => {
+    const like = req.body.like
+    const userId = req.body.userId
+    const sauceId = req.params.id
+
+    switch (like) {
+        case 1:
+            Sauce.updateOne({ _id: sauceId }, { $push: { usersLiked: userId }, $inc: { likes: +1 } })
+                .then(() => res.status(200).json({ message: 'Vous aimez' }))
+                .catch((error) => res.status(400).json({ error }))
+            break
+        case 0:
+            Sauce.findOne({ _id: sauceId })
+                .then((sauce) => {
+                    if (sauce.usersLiked.includes(userId)) {
+                        Sauce.updateOne({ _id: sauceId }, { $pull: { usersLiked: userId }, $inc: { likes: -1 } })
+                            .then(() => res.status(200).json({ message: 'Sans avis' }))
+                            .catch((error) => res.status(400).json({ error }))
+                    }
+                    if (sauce.usersDisliked.includes(userId)) {
+                        Sauce.updateOne({ _id: sauceId }, { $pull: { usersDisliked: userId }, $inc: { dislikes: -1 } })
+                            .then(() => res.status(200).json({ message: 'Sans avis' }))
+                            .catch((error) => res.status(400).json({ error }))
+                    }
+                })
+                .catch((error) => res.status(404).json({ error }))
+            break
+        case -1:
+            Sauce.updateOne({ _id: sauceId }, { $push: { usersDisliked: userId }, $inc: { dislikes: +1 } })
+                .then(() => { res.status(200).json({ message: `Vous n'aimez pas` }) })
+                .catch((error) => res.status(400).json({ error }))
+            break
+        default:
+            console.log(error)
+    }
 }
--- controllers/user.js
+++ controllers/user.js
@@ -21,12 +21,12 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' })
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' })
                     }
                     res.status(200).json({
                         userId: user._id,
@@ -35,9 +35,9 @@ exports.login = (req, res, next) => {
                             'RANDOM_TOKEN_SECRET',
                             { expiresIn: '24h' }
                         )
-                    });
+                    })
                 })
-                .catch(error => res.status(500).json({ error }));
+                .catch(error => res.status(500).json({ error }))
         })
-        .catch(error => res.status(500).json({ error }));
-};
+        .catch(error => res.status(500).json({ error }))
+}
